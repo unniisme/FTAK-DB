db01ee6a83673a53e556257e9bb3b8d8313ca02d
unniisme <122001019@smail.iitpkd.ac.in>
2023-04-29 12:14:15 +0530

    ArrakkalAbu

--- app/Add_user_test.py
+++ app/Add_user_test.py
@@ -11,15 +11,15 @@ db = FTAKdb(user_name=USER_NAME,
                 password=PASSWORD,
                 host=HOST,port=PORT)
 
-db.farmer_sign_up("shajipappan101", "nope", "Shaji", "SS", "20/04/2002", "20/04/2023", "91002918223", 3)
+db.farmer_sign_up("abuarakkal", "atombomb", "Arakkal", "Abu", "21/04/2004", "20/04/2023", "91002918223", 3)
 
 
 """
 --To Delete, run in postgres
-DROP VIEW ShajiPappan101_farmer_info;
-REVOKE ALL ON ALL TABLES IN SCHEMA public FROM shajipappan101;
-DROP ROLE shajipappan101;
-DELETE FROM farmer_login WHERE username = 'shajipappan101';
-DELETE FROM farmer WHERE first_name='Shaji';
+DROP VIEW abuarakkal_farmer_info;
+REVOKE ALL ON ALL TABLES IN SCHEMA public FROM abuarakkal;
+DROP ROLE abuarakkal;
+DELETE FROM farmer_login WHERE username = 'abuarakkal';
+DELETE FROM farmer WHERE first_name='Arakkal';
 ALTER SEQUENCE farmer_farmer_id_seq RESTART WITH 5;
 """
