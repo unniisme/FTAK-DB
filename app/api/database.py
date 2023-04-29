f751c59fcc3ddc2c9542635da542fb97508fbf9b
unniisme <122001019@smail.iitpkd.ac.in>
2023-04-29 18:06:47 +0530

    Fixed query for product and depot fetching

--- app/api/database.py
+++ app/api/database.py
@@ -357,7 +357,7 @@ class FARMERdb(FTAKdb):
         return self.dql_to_dictList(f"SELECT farmer_id, first_name, last_name, dob, doj, phone_number, address_id FROM {self.farmer_info_view};")[0]
 
     def get_depots(self):
-        query = f"SELECT i.depot_id, depot.name, depot.address_id,   FROM {self.farmer_info_view} as i JOIN depot ON i.depot_id = depot.depot.id"
+        query = f"SELECT i.depot_id, depot.name, depot.address_id FROM {self.farmer_info_view} as i JOIN depot ON i.depot_id = depot.depot_id"
         return self.dql_to_dictList(query)
 
     def get_plots(self):
@@ -365,7 +365,7 @@ class FARMERdb(FTAKdb):
         return self.dql_to_dictList(query)
 
     def get_products(self):
-        query = f"SELECT p.product_id, p.name, p.description, p.rate, p.image_link FROM {self.farmer_info_view} as i JOIN product as p ON i.product_id = product.p.product_id"
+        query = f"SELECT p.product_id, p.name, p.description, p.rate, p.image_link FROM {self.farmer_info_view} as i JOIN product as p ON i.product_id = p.product_id"
         return self.dql_to_dictList(query)
         
 
